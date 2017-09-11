a63325d6c61a866dd8f90be049fb945f5cffcf22
juan-esteller <esteller@college.harvard.edu>
2017-09-11 09:24:30 -0400

    removed all files but main.v

--- lab1_project/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/cs141_vm_shared/lab1_project/testbench_isim_beh.exe" -prj "Z:/cs141_vm_shared/lab1_project/testbench_beh.prj" "work.testbench" "work.glbl" 
