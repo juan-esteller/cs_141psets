ac5520333fe5b5d8895a1f1d525398b229d508ca
juan-esteller <esteller@college.harvard.edu>
2017-09-11 12:24:21 -0400

    now have working main.v

--- lab1/main.v
+++ lab1/main.v
@@ -8,19 +8,17 @@ module main(switch, led);
 input  wire [7:0] switch;        //8 input switches
 output wire [7:0] led;           //8 output leds
 
-// cover top left gate 
 assign led[0] = switch[0] ^ switch[1]; 
 
-// cover top right gate 
-assign led[1] = &switch;
+assign led[1] = &switch; 
 
-// wires for circuit of MUXes
+// intermediate wires for MUXes
 wire [5:0] mux_top, mux_bottom; 
 
-assign mux_top = switch[2] ? 6'b101010: 6'b000000;
-assign mux_bottom = switch[3] ? 6'b101010: 6'b111111; 
- 
-assign led[7:2] = switch[4] ? mux_bottom: mux_top; 
+assign mux_top = switch[2]? 6'b101010: 6'b000000; 
+assign mux_bottom = switch[3]? 6'b010101:  6'b111111; 
+
+assign led[7:2] = switch[4]? mux_bottom: mux_top; 
 
 endmodule
 
